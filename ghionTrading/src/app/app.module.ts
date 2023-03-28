905ae1ef4410b1997eaff9cece10df85169052b5
Abel <abelzeleke5173@gmail.com>
2023-03-28 12:17:49 +0300

    first commit

--- ghionTrading/src/app/app.module.ts
+++ ghionTrading/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
