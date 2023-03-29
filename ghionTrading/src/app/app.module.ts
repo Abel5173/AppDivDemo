b4c4782b65ab5c89a75ffdd845fd474ae89455f1
Bereket <berekettadelemulu210@gmail.com>
2023-03-29 12:25:42 +0300

    merge

--- ghionTrading/src/app/app.module.ts
+++ ghionTrading/src/app/app.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
 import { NavbarComponent } from './navbar/navbar.component';
-
-=======
 import { SideBarComponent } from './side-bar/side-bar.component';
-
-
 @NgModule({
   declarations: [
     AppComponent,
 
-    NavbarComponent
+    NavbarComponent,
 
     SideBarComponent
 
